8338f4653c5c1181a8048876c8b9cdb7fc3d5a12
clafouti2104 <nico.gyss@gmail.com>
2014-12-24 10:53:00 +0100

    Index: Enleve Scenario

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $pagesParent=Page::getPageParents();
                 <div id="dashboard">
                         <!-- BEGIN DASHBOARD STATS -->
                         <div class="row">
-                                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box-action" type="freebox">
+                                <!--<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 box-action" type="freebox">
                                         <div class="dashboard-stat blue-madison">
                                                 <div class="visual">
                                                         <i class="fa fa-play-circle"></i>
@@ -99,7 +99,7 @@ $pagesParent=Page::getPageParents();
                                                 Info <i class="m-icon-swapright m-icon-white"></i>
                                                 </a>						
                                         </div>
-                                </div>
+                                </div>-->
 <?php
 foreach($pagesParent as $pageParent){
 ?>
