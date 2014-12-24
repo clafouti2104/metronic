d9cf3e10352234ad90fd40c9eb34a57883669119
clafouti2104 <nico.gyss@gmail.com>
2014-12-24 10:26:04 +0100

    Alarm Home Made

--- alarm.php
+++ alarm.php
@@ -1,35 +1,14 @@
 <?php
 $includeCSS = $includeJS = array();
-$includeJS[] = "/assets/jquery-ui/keyboard/jquery.keyboard.js";
-$includeJS[] = "/assets/jquery-ui/keyboard/jquery.keyboard.extension-typing.js";
+//$includeJS[] = "/assets/jquery-ui/keyboard/jquery.keyboard.js";
+//$includeJS[] = "/assets/jquery-ui/keyboard/jquery.keyboard.extension-typing.js";
 //$includeJS[] = "/assets/jquery-ui/keyboard/jquery.keyboard.extension-autocomplete.js";
 $includeCSS[] = "/assets/jquery-ui/jquery-ui.css";
 include "modules/header.php";
 include "modules/sidebar.php";
 
 $GLOBALS["dbconnec"] = connectDB();
-?>
-<style type="text/css" media="screen">
-.ui-widget-content{
-    background: url("images/ui-bg_inset-soft_25_000000_1x100.png") repeat-x scroll 50% bottom #000000;
-    border: 1px solid #666666;
-    color: #ffffff;
-    <?php if($detect->isMobile()){
-        echo "margin-top:100px;";
-        echo "margin-left:100px;";
-
-    } else {
-        echo "margin-top:200px;";
-        echo "margin-left:250px;";
-
-    } ?>
-
-}
-</style>
-<?php
-
 $ini = parse_ini_file("/var/www/metronic/tools/parameters.ini");
-$ini = parse_ini_file("tools/parameters.ini");
 
 foreach($ini as $title => $value){
     if($title == "myfox_token" && $value != ""){
@@ -148,35 +127,44 @@ if($type=="myfox"){
         </div>
     </div>
     </div>
-</div>
+    <div id="homeKeypad" style="position: absolute; top: 50px;left: 100px; width: 300px; display: block;">
+        <div class="keypadElem ui-dialog-titlebar-close" style="width:225px;height:20px;padding:0;text-align: right;" value="close">
+            x
+        </div>
+        <div class="keypadElem ui-keyboard-button" value="7" style="clear:left;">7</div>
+        <div class="keypadElem ui-keyboard-button" value="8">8</div>
+        <div class="keypadElem" value="9">9</div>
+        <div class="keypadElem" style="clear:left;" value="4">4</div>
+        <div class="keypadElem" value="5">5</div>
+        <div class="keypadElem" value="6">6</div>
+        <div class="keypadElem" value="clear" style="clear:left;">⌫</div>
+        <div class="keypadElem" value="0">0</div>
+        <div class="keypadElem" value="accept" style="background:#f58400;">✔</div>
+    </div>
+    <input type="hidden" id="actionType" value="" />
+    <input type="hidden" id="codePin" value="" />
     <div id="keypad"></div>
+</div>
 <script type="text/javascript">
 $(document).ready(function() {
-    $('#hidden').keyboard({
-        //openOn : null,
-        layout: 'custom',
-        customLayout: {
-           'default' : [
-            '7 8 9',
-            '4 5 6',
-            '1 2 3',
-            '{b} 0 {a}'
-        ]
-        },
-        maxLength : 6,
-        restrictInput : true, // Prevent keys not in the displayed keyboard from being typed in
-        preventPaste : true,  // prevent ctrl-v and right click
-        position: {
-            of : $('.alarm-first'),
-            my : 'center top',
-            at : 'center top'
-        },
-        validate    : function(keyboard, value, isClosing) {
+    $('#homeKeypad').hide();
+    $('.keypadElem').click(function() {
+        if($(this).attr('value') == 'close'){
+            $('#codePin').val('');
+            $('#homeKeypad').hide();
+            return true;
+        }
+        if($(this).attr('value') == 'clear'){
+            $('#codePin').val('');
+            return true;
+        }
+        if($(this).attr('value') == 'accept'){
+            var value=$('#codePin').val();
             $.ajax({
                 url: "ajax/check_keypad_alarm.php    ",
                 type: "POST",
                 data: {
-                    action: $('#action_tmp').val(),
+                    action: $('#actionType').val(),
                     pin: value
                 },
                 error: function(data){
@@ -190,6 +178,7 @@ $(document).ready(function() {
                     }
                     if(data == "codePin"){
                         toastr.error("Mauvais Code");
+                        $('#codePin').val('');
                         value = "";
                         return false;
                     }
@@ -201,81 +190,28 @@ $(document).ready(function() {
                         var exThis=$('.alarm-'+$('#element_tmp').val()).attr('src');
                         $('alarm-'+$('#element_tmp').val()).attr('src',exImgState);
                         $('.alarm-status').attr('src',exThis);
-                        $('.ui-keyboard:not(.ui-keyboard-always-open)').hide();
+                        $('#codePin').val('');
+                        $('#homeKeypad').hide();
+                        //$('.ui-keyboard:not(.ui-keyboard-always-open)').hide();
                     }
                 }
             });
-            value = "";
+            return true;
         }
-    })
-    .addTyping();
+        $('#codePin').val($('#codePin').val()+$(this).attr('value'));
+    });
+    
 
-    $('#hidden2').keyboard({
-        //openOn : null,
-        layout: 'custom',
-        customLayout: {
-           'default' : [
-            '7 8 9',
-            '4 5 6',
-            '1 2 3',
-            '{b} 0 {a}'
-        ]
-        },
-        maxLength : 6,
-        restrictInput : true, // Prevent keys not in the displayed keyboard from being typed in
-        preventPaste : true,  // prevent ctrl-v and right click
-        position: {
-            of : $('.alarm-first'),
-            my : 'center top',
-            at : 'center top'
-        },
-        validate    : function(keyboard, value, isClosing) {
-            $.ajax({
-                url: "ajax/check_keypad_alarm.php    ",
-                type: "POST",
-                data: {
-                    action: $('#action_tmp').val(),
-                    pin: value
-                },
-                error: function(data){
-                    toastr.error("Une erreur est survenue");
-                },
-                complete: function(data){
-                    if(data.responseText == "codePin"){
-                        toastr.error("Mauvais Code");
-                        value = "";
-                        return false;
-                    }
-                    if(data == "codePin"){
-                        toastr.error("Mauvais Code");
-                        value = "";
-                        return false;
-                    }
-                    if(data.responseText == "error"){
-                        toastr.error("Une erreur est survenue");
-                    } else {
-                        toastr.success("Action exécutée");
-                        var exImgState=$('.alarm-status').attr('src');
-                        var exThis=$('.alarm-'+$('#element_tmp').val()).attr('src');
-                        $('alarm-'+$('#element_tmp').val()).attr('src',exImgState);
-                        $('.alarm-status').attr('src',exThis);
-                        $('.ui-keyboard:not(.ui-keyboard-always-open)').hide();
-                    }
-                }
-            });
-            value = "";
-        }
-    })
-    .addTyping();
-
-    $('.ui-keyboard-button').click(function() {
+    /*$('.ui-keyboard-button').click(function() {
         $('.ui-keyboard-preview-wrapper input').focusout();
         $('.ui-keyboard-preview').focusout();
         $("input").blur();
         $('#hidden').blur();
-    });
+    });*/
     $('.alarm-first').click(function() {
-        $("input").blur();
+        $('#homeKeypad').show();
+        $('#actionType').val($(this).attr('action'));
+        /*$("input").blur();
         $('#action_tmp').val($(this).attr('action'));
         $('#element_tmp').val($(this).attr('element'));
         //$('#hidden').trigger('focus.keyboard');
@@ -286,11 +222,13 @@ $(document).ready(function() {
         $('.ui-keyboard-preview').focusout();
         $("input").blur();
         $('.ui-widget-content').css({'margin-top':'150px','margin-left':'50px'});
-        $('.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default').css({'width':'73px','height':'60px'});
+        $('.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default').css({'width':'73px','height':'60px'});*/
     });
 
     $('.alarm-second').click(function() {
-        $("input").blur();
+        $('#homeKeypad').show();
+        $('#actionType').val($(this).attr('action'));
+        /*$("input").blur();
         $('#action_tmp').val($(this).attr('action'));
         $('#element_tmp').val($(this).attr('element'));
         //$('#hidden2').trigger('focus.keyboard');
@@ -301,7 +239,7 @@ $(document).ready(function() {
         $('.ui-keyboard-preview').focusout();
         $("input").blur();
         $('.ui-widget-content').css({'margin-top':'150px','margin-left':'50px'});
-        $('.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default').css({'width':'73px','height':'60px'});
+        $('.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default').css({'width':'73px','height':'60px'});*/
     });
 
     
@@ -317,6 +255,121 @@ $(document).ready(function() {
                 keyboard.style.marginLeft='-131px';
                 keyboard.style.marginTop='-30%';
     });*/
+    /*$('#hidden').keyboard({
+        //openOn : null,
+        layout: 'custom',
+        customLayout: {
+           'default' : [
+            '7 8 9',
+            '4 5 6',
+            '1 2 3',
+            '{b} 0 {a}'
+        ]
+        },
+        maxLength : 6,
+        restrictInput : true, // Prevent keys not in the displayed keyboard from being typed in
+        preventPaste : true,  // prevent ctrl-v and right click
+        position: {
+            of : $('.alarm-first'),
+            my : 'center top',
+            at : 'center top'
+        },
+        validate    : function(keyboard, value, isClosing) {
+            $.ajax({
+                url: "ajax/check_keypad_alarm.php    ",
+                type: "POST",
+                data: {
+                    action: $('#action_tmp').val(),
+                    pin: value
+                },
+                error: function(data){
+                    toastr.error("Une erreur est survenue");
+                },
+                complete: function(data){
+                    if(data.responseText == "codePin"){
+                        toastr.error("Mauvais Code");
+                        value = "";
+                        return false;
+                    }
+                    if(data == "codePin"){
+                        toastr.error("Mauvais Code");
+                        value = "";
+                        return false;
+                    }
+                    if(data.responseText == "error"){
+                        toastr.error("Une erreur est survenue");
+                    } else {
+                        toastr.success("Action exécutée");
+                        var exImgState=$('.alarm-status').attr('src');
+                        var exThis=$('.alarm-'+$('#element_tmp').val()).attr('src');
+                        $('alarm-'+$('#element_tmp').val()).attr('src',exImgState);
+                        $('.alarm-status').attr('src',exThis);
+                        $('.ui-keyboard:not(.ui-keyboard-always-open)').hide();
+                    }
+                }
+            });
+            value = "";
+        }
+    })
+    .addTyping();*/
+
+    /*$('#hidden2').keyboard({
+        //openOn : null,
+        layout: 'custom',
+        customLayout: {
+           'default' : [
+            '7 8 9',
+            '4 5 6',
+            '1 2 3',
+            '{b} 0 {a}'
+        ]
+        },
+        maxLength : 6,
+        restrictInput : true, // Prevent keys not in the displayed keyboard from being typed in
+        preventPaste : true,  // prevent ctrl-v and right click
+        position: {
+            of : $('.alarm-first'),
+            my : 'center top',
+            at : 'center top'
+        },
+        validate    : function(keyboard, value, isClosing) {
+            $.ajax({
+                url: "ajax/check_keypad_alarm.php    ",
+                type: "POST",
+                data: {
+                    action: $('#action_tmp').val(),
+                    pin: value
+                },
+                error: function(data){
+                    toastr.error("Une erreur est survenue");
+                },
+                complete: function(data){
+                    if(data.responseText == "codePin"){
+                        toastr.error("Mauvais Code");
+                        value = "";
+                        return false;
+                    }
+                    if(data == "codePin"){
+                        toastr.error("Mauvais Code");
+                        value = "";
+                        return false;
+                    }
+                    if(data.responseText == "error"){
+                        toastr.error("Une erreur est survenue");
+                    } else {
+                        toastr.success("Action exécutée");
+                        var exImgState=$('.alarm-status').attr('src');
+                        var exThis=$('.alarm-'+$('#element_tmp').val()).attr('src');
+                        $('alarm-'+$('#element_tmp').val()).attr('src',exImgState);
+                        $('.alarm-status').attr('src',exThis);
+                        $('.ui-keyboard:not(.ui-keyboard-always-open)').hide();
+                    }
+                }
+            });
+            value = "";
+        }
+    })
+    .addTyping();*/
 });
 </script>
 <?php
