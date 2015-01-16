dc0e9a3b872593419d401711d08a36d5c59e4360
clafouti2104 <nico.gyss@gmail.com>
2015-01-16 10:45:06 +0100

    Maintenance

---
+++ scripts/delete_old_historique.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Supprime l'historique antérieur au jour (table temperature) pour les devices incrémentaux
+ */
+include("../tools/config.php");
+
+$GLOBALS["dbconnec"] = connectDB();
+
+//Requete SQL
+$sqlHistorique="DELETE FROM temperature WHERE deviceid IN (";
+$sqlHistorique.=" SELECT id FROM device WHERE incremental=1";
+$sqlHistorique.=") ";
+$sqlHistorique.=" AND date < '".date('Y-m-d 00:00:00')."' ";
+$stmt = $GLOBALS["dbconnec"]->prepare($sqlHistorique);
+$stmt->execute(array());
+
+?>
