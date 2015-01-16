343cb7a53306f8d10aae32398ec3b0dbefb176d6
clafouti2104 <nico.gyss@gmail.com>
2015-01-16 10:59:35 +0100

    Save feature

--- ajax/system/save.php
+++ ajax/system/save.php
@@ -2,7 +2,7 @@
 include("../../tools/config.php");
 $GLOBALS["dbconnec"] = connectDB();
 
-exec('mysqldump --defaults-file=/home/manager/.my.cnf -u root '.dbName.' > /var/www/save/domo_'.date('Ymd').'.sql',$output);
+exec('mysqldump --defaults-file=/home/manager/.my.cnf -u domoManager '.dbName.' > /var/www/save/domo_'.date('Ymd').'.sql',$output);
 
 if(file_exists('/var/www/save/domo_'.date('Ymd').'.sql')){
     echo "success";
