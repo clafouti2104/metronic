52306c9d12bf8b64f254ce3563703ea65527599c
clafouti2104 <nico.gyss@gmail.com>
2015-01-19 10:06:08 +0100

    Delete Historique

--- scripts/delete_old_historique.php
+++ scripts/delete_old_historique.php
@@ -11,8 +11,8 @@ $sqlHistorique="DELETE FROM temperature WHERE deviceid IN (";
 $sqlHistorique.=" SELECT id FROM device WHERE incremental=1";
 $sqlHistorique.=") ";
 $sqlHistorique.=" AND date < '".date('Y-m-d 00:00:00')."' ";
-/*$stmt = $GLOBALS["dbconnec"]->prepare($sqlHistorique);
-$stmt->execute(array());*/
-echo $sqlHistorique;
+$stmt = $GLOBALS["dbconnec"]->prepare($sqlHistorique);
+$stmt->execute(array());
+//echo $sqlHistorique;
 
 ?>
