c915e417e5538a6ad642238a8156d3d4067aa973
clafouti2104 <nico.gyss@gmail.com>
2015-02-13 11:17:53 +0100

    RT

--- page_chart.php
+++ page_chart.php
@@ -3,7 +3,7 @@ $chart=Chart::getChart($item->chartId);
 $heightChart=$chart->size*80;
 ?>
 <div class="cell col-lg-<?php echo $chart->size; ?> col-md-6 col-sm-6 col-xs-12 boxPackery itempage itempage-<?php echo $item->id; ?>" iditempage="<?php echo $item->id; ?>" style="height:400px;">
-    <div class="container-<?php echo $item->id; ?> dashboard-stat">
+    <div id="container-<?php echo $item->id; ?>" class="container-<?php echo $item->id; ?> dashboard-stat">
     </div>
     <div class="deleteItemPage" style="margin-bottom:10px;position: absolute;top:370px;right:10px;">
         <a class=" btnDeletePageItem" iditempage="<?php echo $item->id; ?>" data-toggle="modal" href="page.php#deleteItemPage">
