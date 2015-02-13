124edb7cec5b90e132945b23aad983d1ff1a0bf6
clafouti2104 <nico.gyss@gmail.com>
2015-02-13 13:57:49 +0100

    RT

--- controllers/live-server-data.php
+++ controllers/live-server-data.php
@@ -82,8 +82,8 @@ foreach($chartDevices as $chartDevice){
         //$output .= "";
         //$output .= "var series = chart".$_POST["itemId"].".series[0];";
         $output .= "var shift = chart".$_POST["itemId"].".series[".$i."].data.length > 50;";
-        $output .= "var dateTmp=new Date();";
-        $output .= "dateTmp += 60;";
+        $output .= "var dateTmp=Date.now();";
+        $output .= "dateTmp += 3600;";
         $output .= "chart".$_POST["itemId"].".series[".$i."].addPoint([dateTmp, ".intval($value)."],true,shift);";
     }
     $value="";
