3d501aed0c003ce6af9333e8e076a0280aa0a1a2
clafouti2104 <nico.gyss@gmail.com>
2015-06-27 18:57:09 +0200

    Create Table

--- scripts/createTableMonth.php
+++ scripts/createTableMonth.php
@@ -21,14 +21,18 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
   `date` datetime NOT NULL,
   `value` double NOT NULL,
   `deviceid` int(11) DEFAULT NULL,
-  `calaosid` int(11) DEFAULT NULL) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;";
+  `calaosid` int(11) DEFAULT NULL,
+  PRIMARY KEY (id) ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;";
+        $sqlTable .= "ALTER TABLE `temperature_".$row["id"]."` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;";
     }
     if(!is_null($row['incremental']) && $row['incremental'] != ""){
         $sqlTable .= "CREATE TABLE IF NOT EXISTS releve_".$row["id"]." (
   `id` mediumint(9) NOT NULL,
   `value` float DEFAULT NULL,
-  `date` datetime DEFAULT NULL    
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (id)
         )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;";
+        $sqlTable .= "ALTER TABLE `releve_".$row["id"]."` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;";
     }
 }
 
