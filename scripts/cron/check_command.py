21230792b3892ca05c3674bab843b5a637e50e53
clafouti2104 <nico.gyss@gmail.com>
2015-10-15 18:24:29 +0200

    cmd

--- scripts/cron/check_command.py
+++ scripts/cron/check_command.py
@@ -5,9 +5,9 @@ Check If Command To Execute
 """
 
 import sys
-import file
 import os.path
 
 if os.path.isfile("/etc/domokine/command"):
 	file = open('/etc/domokine/command', 'r')
-	print file.read()
+	command=file.read()
+	exec(command)
